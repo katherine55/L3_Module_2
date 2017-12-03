017d8b262b90afff272414e3ec8fd8359ee3c65f
DavidAndBlake <League@AdministorsiMac.attlocal.net>
2017-12-03 11:26:55 -0800

    Nuevo

--- src/intro_to_file_io/Decrypted.java
+++ src/intro_to_file_io/Decrypted.java
@@ -1,16 +1,54 @@
 package intro_to_file_io;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+
 public class Decrypted {
 
-	String decrypt(String x) {
+	public static void main(String[] args) {
+
+		String fileName = "";
+		JFileChooser jfc = new JFileChooser();
+		int returnVal = jfc.showOpenDialog(null);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			fileName = jfc.getSelectedFile().getAbsolutePath();
+			System.out.println(fileName);
+		}
+
+		Decrypted chuck = new Decrypted();
+		chuck.decrypt(fileName);
+	}
+
+	String decrypt(String n) { // input: file name
+		String x = null;
 		String decoded = "";
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(n));
+			x = br.readLine();
+			br.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		decoded = "";
 		String[] numbers = x.split(" ");
-		for(int i = 0; i < numbers.length; i++) {
+		for (int i = 0; i < numbers.length; i++) {
 			int ascii = Integer.parseInt(numbers[i]);
-			decoded+= (char) ascii;
-		}	
+			decoded += (char) ascii;
+		}
+		
 		System.out.println(decoded);
+
 		return decoded;
 	}
-	
+
 }
